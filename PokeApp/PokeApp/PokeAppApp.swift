f1bcef1c18e5d848e379ff769eabfa77e465e4ff
Aldair Cosetito <aldair.loq.369@gmail.com>
2024-12-22 00:37:03 -0500

    feat: Initial commit.

---
+++ PokeApp/PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Aldair Cosetito Coral on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
